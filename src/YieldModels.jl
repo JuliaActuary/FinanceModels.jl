3926817313a3ff7f37b87515fd01d01206dfd25f
Alec Loudenback <alecloudenback@gmail.com>
2020-06-28 22:47:58 -0500

    initial version

--- src/YieldModels.jl
+++ src/YieldModels.jl
@@ -1,5 +1,60 @@
 module YieldModels
 
-# Write your package code here.
+using Dierckx
+
+export ZeroCurve, rate,forward,TreasuryYieldCurve
+
+
+struct YieldCurve
+    rates
+    maturities
+    spline
+end
+
+function ZeroCurve(rates,maturities)
+    return YieldCurve(rates,maturities,Spline1D(maturities,rates))
+end
+
+function TreasuryYieldCurve(rates,maturities)
+    z = zeros(length(rates))
+
+    # use the discount rate for T-Bills with maturities <= 1 year
+    for (i,(rate,mat)) in enumerate(zip(rates,maturities))
+        
+        if mat <= 1 
+            z[i] = rate
+        else
+            i, rate, mat
+            curve = Spline1D(maturities,z)
+            pmts = [rate / 2 for t in 0.5:0.5:mat] # coupons only
+            pmts[end] += 1 # plus principal
+
+            disc =  1 ./ (1 .+ curve.(0.5:0.5:(mat - .5)))
+            z[i] = ((1 - sum(disc .* pmts[1:end-1])) / pmts[end]) ^ - (1/mat) - 1
+
+        end
+
+
+
+
+        
+    end
+
+    return YieldCurve(rates,maturities,Spline1D(maturities,z))
+
+
+end
+
+function ParYieldCurve(rates,maturities)
+
+end
+
+rate(yc,time) = yc.spline(time)
+
+disc(yc,time) = 1 / (1 + rate(yc,time)) ^ time
+
+function forward(yc,from,to) 
+    (rate(yc,to) * to - rate(yc,from) * from) / (to - from)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,48 @@ using YieldModels
 using Test
 
 @testset "YieldModels.jl" begin
-    # Write your tests here.
+    @testset "simple rate and forward" begin 
+    # Risk Managment and Financial Institutions, 5th ed. Appendix B
+
+        maturity = [0.5, 1.0, 1.5, 2.0]
+        zero    = [5.0, 5.8, 6.4, 6.8] ./ 100
+        curve = ZeroCurve(zero,maturity)
+
+        
+        @test rate(curve,0.5) ≈ 5.0 / 100
+        @test rate(curve,2.0) ≈ 6.8 / 100
+
+        @test disc(curve, 1) ≈ 1 / (1 + zero[2])
+        @test disc(curve, 2) ≈ 1 / (1 + zero[4]) ^2
+    
+        # extrapolation
+        @test rate(curve,0.0) ≈ 5.0 / 100
+        @test rate(curve,4.0) ≈ 6.8 / 100
+
+        @test forward(curve,0.5,1.0) ≈ 6.6 / 100
+        @test forward(curve,1.0,1.5) ≈ 7.6 / 100
+        @test forward(curve,1.5,2.0) ≈ 8.0 / 100
+    end
+
+    @testset "bootstrap treasury" begin
+        # https://financetrain.com/bootstrapping-spot-rate-curve-zero-curve/
+        maturity = [0.5, 1.0, 1.5, 2.0]
+        YTM      = [4.0, 4.3, 4.5, 4.9] ./ 100
+
+        curve = TreasuryYieldCurve(YTM,maturity)
+
+        @test rate(curve, 0.5) ≈ 0.04
+        @test rate(curve, 1.0) ≈ 0.043
+
+        # need more future tests, but need validating source...
+
+    end
+
+    @testset "actual cmt treasury" begin
+        
+        cmt  = [0.12,0.15,0.14,0.17,0.17,0.17,0.19,0.30,0.49,0.64,1.15,1.37] ./ 100
+        mats =  [1/12,2/12,3/12,6/12,1,2,3,5,7,10,20,30]
+
+    end
+
 end
