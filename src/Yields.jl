1794a82e904ca34526d87ae1c219771100db427a
Alec Loudenback <alecloudenback@gmail.com>
2021-04-04 22:25:11 -0500

    add `Rate` and `convert`

--- src/Yields.jl
+++ src/Yields.jl
@@ -4,7 +4,7 @@ import Interpolations
 
 # don't export type, as the API of Yields.Zero is nicer and 
 # less polluting than Zero and less/equally verbose as ZeroYieldCurve or ZeroCruve
-export rate, discount, accumulation,forward, Yield
+export rate, discount, accumulation,forward, Yield, Rate
 # USTreasury,  AbstractYield
 # Zero,Constant, Forward
 """
@@ -22,6 +22,23 @@ struct Continuous <: CompoundingFrequency end
 
 struct Periodic <: CompoundingFrequency
     frequency::Int
+end
+
+struct Rate
+    compounding
+    value
+end
+
+Rate(x) = Rate(Periodic(1),x)
+Base.convert(T::CompoundingFrequency,r::Rate) = Base.convert(r.compounding,T,r)
+function Base.convert(from::Continuous,to::Continuous,r)
+    return r.value
+end
+function Base.convert(from::Continuous,to::Periodic,r)
+    to.frequency * (exp(r.value/to.frequency) - 1)
+end
+function Base.convert(from::Periodic,to::Continuous,r)
+    from.frequency * log(1 + r.value / from.frequency)
 end
 
 # make interest curve broadcastable so that you can broadcast over multiple`time`s in `interest_rate`
