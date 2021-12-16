378a47afedb9cbd12979f69fad13505d1dc1753d
alecloudenback <alecloudenback@users.noreply.github.com>
2021-12-16 18:16:19 -0500

    fix `show` methods to extend `Base.show` (#71)

--- src/Yields.jl
+++ src/Yields.jl
@@ -282,8 +282,8 @@ function Constant(rate::T, cf::C = Periodic(1)) where {T<:Real,C<:CompoundingFre
     return Constant(Rate(rate, cf))
 end
 
-zero(c::Constant, time) = c.rate
-zero(c::Constant, time, cf::C) where {C<:CompoundingFrequency} = convert(cf, c.rate)
+Base.zero(c::Constant, time) = c.rate
+Base.zero(c::Constant, time, cf::C) where {C<:CompoundingFrequency} = convert(cf, c.rate)
 rate(c::Constant) = c.rate
 rate(c::Constant, time) = c.rate
 discount(c::T, time) where {T<:Real} = discount(Constant(c), time)
