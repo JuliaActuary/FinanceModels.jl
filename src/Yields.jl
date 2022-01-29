3a9b1103c903e91f869c5dc6cf6dbc306b14c792
alecloudenback <alecloudenback@users.noreply.github.com>
2022-01-28 22:32:35 -0600

    remove duplicate `rate`

--- src/Yields.jl
+++ src/Yields.jl
@@ -7,7 +7,7 @@ using UnicodePlots
 
 # don't export type, as the API of Yields.Zero is nicer and 
 # less polluting than Zero and less/equally verbose as ZeroYieldCurve or ZeroCurve
-export rate, discount, accumulation, forward, rate, spot
+export rate, discount, accumulation, forward, spot
 
 include("Rate.jl")
 include("AbstractYield.jl")
