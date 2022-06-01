4dbcc13f23ffcde8b16cce69a6d273d104c13b09
Alec Loudenback <alecloudenback@gmail.com>
2022-05-31 22:26:53 -0500

    parameterize methods for performance; use default compounding

--- src/generics.jl
+++ src/generics.jl
@@ -14,16 +14,16 @@ discount(yc::AbstractYield, from, to) = discount(yc, to) / discount(yc, from)
 
 The forward `Rate` implied by the yield curve `yc` between times `from` and `to`.
 """
-function forward(yc::AbstractYield, from, to)
-    return forward(yc, from, to, Periodic(1))
+function forward(yc::T, from, to) where {T<:AbstractYield}
+    return forward(yc, from, to, DEFAULT_COMPOUNDING)
 end
 
-function forward(yc::AbstractYield, from, to, cf::CompoundingFrequency)
+function forward(yc::T, from, to, cf::CompoundingFrequency) where {T<:AbstractYield}
     r = Periodic((accumulation(yc, to) / accumulation(yc, from))^(1 / (to - from)) - 1, 1)
     return convert(cf, r)
 end
 
-function forward(yc::AbstractYield, from)
+function forward(yc::T, from) where {T<:AbstractYield}
     to = from + 1
     return forward(yc, from, to)
 end
