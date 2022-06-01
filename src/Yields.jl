33a76889a95f49a8e32cf886a8b49db11a031d4e
Alec Loudenback <alecloudenback@gmail.com>
2022-05-31 22:25:03 -0500

    define global default rate type

--- src/Yields.jl
+++ src/Yields.jl
@@ -6,6 +6,8 @@ using LinearAlgebra
 using UnicodePlots
 using Roots
 
+const DEFAULT_COMPOUNDING = Continuous()
+
 # don't export type, as the API of Yields.Zero is nicer and 
 # less polluting than Zero and less/equally verbose as ZeroYieldCurve or ZeroCurve
 export rate, discount, accumulation, forward,
