dcb53ead60e68f1e347d89928d14f6add3915df8
Alec Loudenback <alecloudenback@gmail.com>
2022-06-12 09:20:26 -0500

    delete unused code, make nss_params an alternate method for NSS

--- test/NelsonSiegelSvensson.jl
+++ test/NelsonSiegelSvensson.jl
@@ -8,7 +8,7 @@
             euraaa_pars = [-0.601861,-0.612808,-0.621403,-0.627731,-0.63251,-0.610271,-0.568825,-0.515067,-0.454526,-0.391338,-0.328412,-0.26767,-0.210277,-0.156851,-0.107632,-0.062605,-0.021601,0.015642,0.049427,0.080073,0.107892,0.133178,0.156206,0.17722,0.196444,0.214073,0.230281,0.245221,0.259027,0.271818,0.283696,0.294753,0.305069] ./ 100
             euraaa_maturities = vcat([0.25, 0.5, 0.75], 1:30)
             ns_param = Yields.NelsonSiegel(0.6202603126029489 /100, -1.1621281759833935 /100, -1.930016080035979 /100, 3.0)
-            ns = Yields.est_ns_params(euraaa_zeros, euraaa_maturities)
+            ns = Yields.NelsonSiegel(euraaa_zeros, euraaa_maturities)
             @testset "parameter: $param" for param in [:β₀, :β₁, :β₂, :τ₁]
                 @test getfield(ns, param) ≈ getfield(ns_param, param)
             end
@@ -47,7 +47,7 @@
             euraaa_pars = [-0.601861,-0.612808,-0.621403,-0.627731,-0.63251,-0.610271,-0.568825,-0.515067,-0.454526,-0.391338,-0.328412,-0.26767,-0.210277,-0.156851,-0.107632,-0.062605,-0.021601,0.015642,0.049427,0.080073,0.107892,0.133178,0.156206,0.17722,0.196444,0.214073,0.230281,0.245221,0.259027,0.271818,0.283696,0.294753,0.305069] ./ 100
             euraaa_maturities = vcat([0.25, 0.5, 0.75], 1:30)
             nss_param = Yields.NelsonSiegelSvensson(0.6348554895516274, -1.2260341163477764, -2.1112390509907732, 0.2927644328902819, 3.0, 1.5)
-            nss = Yields.est_nss_params(euraaa_zeros, euraaa_maturities)
+            nss = Yields.NelsonSiegelSvensson(euraaa_zeros, euraaa_maturities)
             # @testset "parameter: $param" for param in [:β₀, :β₁, :β₂, :β₃, :τ₁, :τ₂]
             #     # the parameters are based on percentages not rates 
             #     @test_broken getfield(nss, param) ≈ getfield(nss_param, param)
