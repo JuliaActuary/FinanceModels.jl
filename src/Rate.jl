92e8078e65ab9eed7cdaf1959076912f5cb5bed1
alecloudenback <alecloudenback@users.noreply.github.com>
2022-06-14 22:13:41 -0500

    document `rate`

--- src/Rate.jl
+++ src/Rate.jl
@@ -188,7 +188,19 @@ end
 
 
 """ 
-    
+    rate(r::Rate)
+
+Returns the untyped scalar interest rate represented by the `Rate`.
+
+# Examples
+
+```julia-repl
+julia> r =Continuous(0.03)
+Yields.Rate{Float64, Continuous}(0.03, Continuous())
+
+julia> rate(r)
+0.03
+```
 """
 function rate(r::Rate{<:Real,<:CompoundingFrequency})
     r.value
