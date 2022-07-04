87d77a834a3dc4fae8d4145228aa718929ad9326
alecloudenback <alecloudenback@users.noreply.github.com>
2022-07-03 21:22:52 -0600

    let `Periodic` work for numbers approximately equal to integers (#130)

--- test/misc.jl
+++ test/misc.jl
@@ -37,3 +37,14 @@ end
     @test Yields.__coerce_rate(0.05, Periodic(1)) == Periodic(0.05,1)
     @test Yields.__coerce_rate(Periodic(0.05,12), Periodic(1)) == Periodic(0.05,12)
 end
+
+#Issue #117
+@testset "DecFP" begin
+    import DecFP
+
+    @test Yields.Periodic(1/DecFP.Dec64(1/6)) == Yields.Periodic(6)
+    mats = convert.(DecFP.Dec64,[1/12, 2/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30])
+    rates = convert.(DecFP.Dec64,[0.01, 0.01, 0.03, 0.05, 0.07, 0.16, 0.35, 0.92, 1.40, 1.74, 2.31, 2.41] ./ 100)
+    y = Yields.CMT(rates,mats)
+    @test y isa Yields.AbstractYieldCurve
+end
