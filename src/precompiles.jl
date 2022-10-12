80799c4871a50de3d1c73f13ff3a4f34d9ae3c81
Alec Loudenback <alecloudenback@gmail.com>
2022-10-11 22:13:04 -0500

    use SnoopPrecompile

--- src/precompiles.jl
+++ src/precompiles.jl
@@ -1,9 +1,15 @@
-function _precompile_()
-    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
 
-    let 
-        rates = [0.057, 0.0755, 0.0837, 0.078, 0.1084, 0.0702, 0.1167]
-        tenors = [1,3,5,10,15,20,25]
+# created with the help of SnoopCompile.jl
+@precompile_setup begin
+
+    
+    # 2021-03-31 rates from Treasury.gov
+    rates =[0.01, 0.01, 0.03, 0.05, 0.07, 0.16, 0.35, 0.92, 1.40, 1.74, 2.31, 2.41] ./ 100
+    tenors = [1/12, 2/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30]
+
+    @precompile_all_calls begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
         Yields.Par(rates,tenors)
         Yields.CMT(rates,tenors)
         Yields.Forward(rates,tenors)
@@ -16,17 +22,5 @@ function _precompile_()
         Yields.par(c,10)
         Yields.forward(c,5,6)
     end
-    let 
-        rates = [0.057, 0.0755, 0.0837, 0.078, 0.1084, 0.0702, 0.1167]
-        tenors = [1.0,3,5,10,15,20,25]
-        Yields.Par(rates,tenors)
-        Yields.CMT(rates,tenors)
-        Yields.Forward(rates,tenors)
-        Yields.OIS(rates,tenors)
-        Yields.Zero(rates,tenors)
-        Yields.Zero(NelsonSiegel(), rates,tenors)
-        Yields.Zero(NelsonSiegelSvensson(), rates,tenors)
-                
-    end
-    
+        
 end
