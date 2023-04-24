d9e100f1419d5950f9584ca60f3b1cb97da52583
Tim Holy <tim.holy@gmail.com>
2023-04-24 16:31:57 -0500

    Migrate from SnoopPrecompile to PrecompileTools

--- src/precompiles.jl
+++ src/precompiles.jl
@@ -1,13 +1,13 @@
 
 # created with the help of SnoopCompile.jl
-@precompile_setup begin
+@setup_workload begin
 
     
     # 2021-03-31 rates from Treasury.gov
     rates =[0.01, 0.01, 0.03, 0.05, 0.07, 0.16, 0.35, 0.92, 1.40, 1.74, 2.31, 2.41] ./ 100
     tenors = [1/12, 2/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30]
 
-    @precompile_all_calls begin
+    @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
         Yields.Par(rates,tenors)
