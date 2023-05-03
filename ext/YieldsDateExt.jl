5985221164f4ffd05c623084832c9f94d2e6fe88
Alec Loudenback <alecloudenback@gmail.com>
2023-05-02 23:23:14 -0500

    date ext

---
+++ ext/YieldsDateExt.jl
@@ -0,0 +1,19 @@
+module YieldsDateExt
+
+using Dates
+using DayCounts
+
+struct DateCurve{T,D<:DayCounts.AbstractDayCount}
+    curve::T
+    dc::D
+    reference_date::Date
+
+    function new(curve::T,dc::D=DayCounts.ActualActual365, reference_date::Date=today() ) where {T}
+        return new{T,D}(curve, dc, reference_date)
+    end
+end
+
+discount(c::DateCurve, date::Date) = discount(c.curve, time, c.dc)
+
+
+end
