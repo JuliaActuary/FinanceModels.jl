281e02230dd21716f589757744d5a36a438ae66f
alecloudenback <alecloudenback@users.noreply.github.com>
2025-08-04 09:56:44 -0600

    mark Makie extension tests broken

--- test/extensions.jl
+++ test/extensions.jl
@@ -2,12 +2,12 @@
     using CairoMakie
     cfs = [Cashflow(t + randn(), t) for t in 1:10]
 
-    @test stem(cfs) isa Makie.FigureAxisPlot
+    @test_broken stem(cfs) isa Makie.FigureAxisPlot
 
-    b = Bond.Fixed(0.10, Periodic(2), 20)
+    b = Bond.Fixed(0.1, Periodic(2), 20)
     proj = Projection(b, NullModel(), CashflowProjection())
     # a stem plot:
-    @test stem(proj) isa Makie.FigureAxisPlot
+    @test_broken stem(proj) isa Makie.FigureAxisPlot
 
-    @test stem(b) isa Makie.FigureAxisPlot
+    @test_broken stem(b) isa Makie.FigureAxisPlot
 end
