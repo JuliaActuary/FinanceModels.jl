54bab8969665d4f391120904ef15375f626c0326
alecloudenback <alecloudenback@users.noreply.github.com>
2026-08-06 13:49:07 -0600

    Support DataInterpolations 9 (#293)
    * Support DataInterpolations 9
    
    * Require DataInterpolations 9

--- src/model/Yield.jl
+++ src/model/Yield.jl
@@ -81,7 +81,7 @@ function Spline(b::Sp.BSpline, xs, ys)
         :Average
     end
 
-    return Spline(DataInterpolations.BSplineInterpolation(ys, xs, order, :Uniform, knot_type; extrapolation = DataInterpolations.ExtrapolationType.Extension))
+    return Spline(DataInterpolations.BSplineInterpolation(ys, xs, order, knot_type; extrapolation = DataInterpolations.ExtrapolationType.Extension))
 end
 
 function Spline(::Sp.PCHIP, xs, ys)
